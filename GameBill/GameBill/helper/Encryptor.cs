e617cffc27018fb9d3a5cedc78f2975574a7aeca
Denaya Dwiagnes <denaya.dwiagnes@gmail.com>
2024-06-10 22:31:52 +0700

    Create helper, auth folder & Encryptor cs, login aspx

---
+++ GameBill/GameBill/helper/Encryptor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace GameBill.helper
+{
+    public class Encryptor
+    {
+        public string Hash(string input)
+        {
+            var bytes = Encoding.UTF8.GetBytes(input);
+            byte[] hashBytes;
+
+            using (var algo = new System.Security.Cryptography.SHA512Managed())
+            {
+                hashBytes = algo.ComputeHash(bytes);
+            }
+
+            return Convert.ToBase64String(hashBytes);
+        }
+    }
+}
